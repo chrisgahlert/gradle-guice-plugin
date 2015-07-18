9a53715824f1de572350c1e1130bbba953172f73
Christian Gahlert <extr3m0@email.de>
2015-07-18 12:00:00 +0200

    Added Gradle plugin publishing

---
+++ src/main/groovy/com/chrisgahlert/gradleguiceplugin/gradle/DefaultPlugin.groovy
@@ -0,0 +1,12 @@
+package com.chrisgahlert.gradleguiceplugin.gradle
+
+import org.gradle.api.Project
+
+/**
+ * Created by Chris on 22.07.2015.
+ */
+class DefaultPlugin extends GuicePlugin {
+    @Override
+    void doApply(Project project) {
+    }
+}
