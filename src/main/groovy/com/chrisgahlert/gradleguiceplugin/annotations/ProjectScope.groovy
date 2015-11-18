21e8df05bc9d656c3356e01ef8ea02b7c80589a8
extr3m0 <extr3m0@email.de>
2015-11-18 22:41:55 +0100

    Changed the annotation class file from Java to Groovy

---
+++ src/main/groovy/com/chrisgahlert/gradleguiceplugin/annotations/ProjectScope.groovy
@@ -0,0 +1,18 @@
+package com.chrisgahlert.gradleguiceplugin.annotations
+
+import com.google.inject.ScopeAnnotation
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+/**
+ * This annotation makes use of the Gradle project scope in Guice dependency injection.
+ * All classes annotated with this will only have 1 instance per Gradle Project.
+ */
+@Target([ElementType.TYPE, ElementType.METHOD])
+@Retention(RetentionPolicy.RUNTIME)
+@ScopeAnnotation
+@interface ProjectScope {
+}
